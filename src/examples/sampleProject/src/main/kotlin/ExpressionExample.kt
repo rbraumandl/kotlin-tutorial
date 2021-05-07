68d921bb6eb17994cf5c56b8d298d267d6925f14
Reinhard Braumandl <reinhard@braumandl.de>
2021-05-08 00:08:23 +0200

    Klassen und Sonderformen davon beschrieben

---
+++ src/examples/sampleProject/src/main/kotlin/ExpressionExample.kt
@@ -0,0 +1,12 @@
+class ExpressionExample {
+
+    fun main(){
+        val x = 5
+        val y = 3
+        when {
+            x > 4 -> "x größer 4"
+            y < 4 -> "y kleiner 4"
+            else -> "weiß nicht"
+        }
+    }
+}
